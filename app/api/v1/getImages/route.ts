324862a1ac832508b445af2339fededfc4cdc30d
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-09 23:40:44 +0800

    add server metre

--- app/api/v1/getImages/route.ts
+++ app/api/v1/getImages/route.ts
@@ -5,8 +5,10 @@ import { respData, respErr } from "@/lib/resp";
 import { auth, clerkClient } from "@clerk/nextjs";
 export async function GET(req: Request) {
     try {
+        console.log('getImages server 接收到了')
         const body = await req.text();
         const res = await getImages()
+        console.log('getImages server 调用数据库好了')
         return respData(res)
     } catch (e) {
         console.log("getImages failed");
