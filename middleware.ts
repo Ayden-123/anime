e5f2b363ba182111e2b30325abe7f4aef96d9858
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-13 21:27:31 +0800

    add log

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
+import { getUuid } from "./lib";
 import { getLocale, locales } from "./lib/i18n";
 
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe',
-    '/api/v1/getUserInfo', 'en', 'zh'],
+    '/api/v1/getUserInfo', '/en', '/zh'],
 
   afterAuth(auth, req, evt) {
+
     const { pathname } = req.nextUrl;
+    
+    const uuid = getUuid()
+    console.log(uuid + 'reqname为' + pathname)
+
     if (pathname === "/favicon.ico" || pathname.startsWith("/api/")) {
       return;
     }
@@ -18,13 +24,15 @@ export default authMiddleware({
     const pathnameHasLocale = locales.some(
       (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
     );
-
+    console.log(uuid + 'pathnameHasLocale为' + pathnameHasLocale)
     if (pathnameHasLocale) return;
 
     const locale = getLocale({
       "accept-language": req.headers.get("accept-language"),
     });
 
+    console.log(uuid + 'nextReq为' + `/${locale}${pathname}`)
+    
     req.nextUrl.pathname = `/${locale}${pathname}`;
 
     return Response.redirect(req.nextUrl);
