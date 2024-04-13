3f042842a35994c2992f5d99d44718b13295456b
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-14 00:33:58 +0800

    test3

--- middleware.ts
+++ middleware.ts
@@ -11,29 +11,29 @@ export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk',
     '/api/v1/getUserInfo'],
 
-  // afterAuth(auth, req, evt) {
-  //   const { pathname } = req.nextUrl;
-  //   if (pathname === "/favicon.ico" || pathname.startsWith("/api/")) {
-  //     return;
-  //   }
-  //   if (pathname.includes("sign-in") || pathname.includes("sign-up") ) {
-  //     return;
-  //   }
+  afterAuth(auth, req, evt) {
+    const { pathname } = req.nextUrl;
+    if (pathname === "/favicon.ico" || pathname.startsWith("/api/")) {
+      return;
+    }
+    if (pathname.includes("sign-in") || pathname.includes("sign-up") ) {
+      return;
+    }
 
-  //   const pathnameHasLocale = locales.some(
-  //     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  //   );
-  //   if (pathnameHasLocale) {
-  //     return ;
-  //   }
+    const pathnameHasLocale = locales.some(
+      (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    );
+    if (pathnameHasLocale) {
+      return ;
+    }
 
   //   const locale = getLocale({
   //     "accept-language": req.headers.get("accept-language"),
   //   });
   //   req.nextUrl.pathname = `/${locale}${pathname}`;
 
-  //   return Response.redirect(req.nextUrl);
-  // },
+    return Response.redirect(req.nextUrl);
+  },
 });
 
 export const config = {
