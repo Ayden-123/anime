e07863847c3345efe9a71fcb90bfdc8be9fa5aec
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-13 17:31:31 +0800

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe',
-    '/api/v1/getUserInfo'],
+    '/api/v1/getUserInfo', '/en'],
 
   afterAuth(auth, req, evt) {
-    // const { pathname } = req.nextUrl;
-    // // 路由请求可以通过，类似/en
-    // if (!pathname.startsWith("/api")) {
-    //   return NextResponse.next();
-    // }
+    const { pathname } = req.nextUrl;
+    // 路由请求可以通过，类似/en
+    if (!pathname.startsWith("/api")) {
+      return NextResponse.next();
+    }
 
     // if (!auth.userId && !auth.isPublicRoute) {
     //   if (auth.isApiRoute) {
