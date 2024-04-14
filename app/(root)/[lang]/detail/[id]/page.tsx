09ea4aa63a1d82ee50b91b2fb422e4f9bf183469
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-15 01:10:01 +0800

    fix meta

--- app/(root)/[lang]/detail/[id]/page.tsx
+++ app/(root)/[lang]/detail/[id]/page.tsx
@@ -1,6 +1,28 @@
 import Detail from "@/components/shared/Detail";
 import { getDictionary } from "@/lib/i18n";
+import { siteConfig } from "@/lib/site";
 import { getImageDetailed } from '@/models/image';
+import { Metadata } from "next";
+
+export async function generateMetadata({
+    params,
+  }: {
+    params: { lang: string; id: string }
+  }): Promise<Metadata> {
+    const dict = await getDictionary(params.lang);
+  
+    let description = ""
+    if (params.id) {
+        let image: Image = await getImageDetailed(params.id)
+        if (image) {
+            description = image.prompt
+        }
+    }
+
+    return {
+      description: `${dict.meta.meta_title} | ${description}`,
+    };
+  }
 
 const DetailPage = async ({ params }: { params: { lang: string; id: string } }) => {
     const dict = await getDictionary(params.lang)
