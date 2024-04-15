5dc4eb1405498d9847101008df53b7eec179d1e3
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-15 11:38:16 +0800

    convert auth

--- middleware.ts
+++ middleware.ts
@@ -3,49 +3,49 @@ import { NextRequest, NextResponse } from "next/server";
 import { getUuid } from "./lib";
 import { getLocale, locales } from "./lib/i18n";
 
-export function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
-  if (pathname === "/favicon.ico" || pathname.startsWith("/api/")) {
-    return;
-  }
-  if (pathname.includes("sign-in") || pathname.includes("sign-up") ) {
-    return;
-  }
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  );
-  if (pathnameHasLocale) {
-    return ;
-  }
-  req.nextUrl.pathname = `/en${pathname}`;
+// export function middleware(req: NextRequest) {
+//   const { pathname } = req.nextUrl;
+//   if (pathname === "/favicon.ico" || pathname.startsWith("/api/")) {
+//     return;
+//   }
+//   if (pathname.includes("sign-in") || pathname.includes("sign-up") ) {
+//     return;
+//   }
+//   const pathnameHasLocale = locales.some(
+//     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+//   );
+//   if (pathnameHasLocale) {
+//     return ;
+//   }
+//   req.nextUrl.pathname = `/en${pathname}`;
 
-  return Response.redirect(req.nextUrl);
-}
+//   return Response.redirect(req.nextUrl);
+// }
 
-// export default authMiddleware({
-//   publicRoutes: ['/', '/api/webhooks/clerk',
-//     '/api/v1/getUserInfo'],
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk',
+    '/api/v1/getUserInfo'],
 
-//   afterAuth(auth, req, evt) {
+  afterAuth(auth, req, evt) {
 
-//     const { pathname } = req.nextUrl;
-//     if (pathname === "/favicon.ico" || pathname.startsWith("/api/")) {
-//       return;
-//     }
-//     if (pathname.includes("sign-in") || pathname.includes("sign-up") ) {
-//       return;
-//     }
+    const { pathname } = req.nextUrl;
+    if (pathname === "/favicon.ico" || pathname.startsWith("/api/")) {
+      return;
+    }
+    if (pathname.includes("sign-in") || pathname.includes("sign-up") ) {
+      return;
+    }
 
-//     const pathnameHasLocale = locales.some(
-//       (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-//     );
-//     if (pathnameHasLocale) {
-//       return ;
-//     }
-//     req.nextUrl.pathname = `/en${pathname}`;
-//     return Response.redirect(req.nextUrl);
-//   },
-// });
+    const pathnameHasLocale = locales.some(
+      (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    );
+    if (pathnameHasLocale) {
+      return ;
+    }
+    req.nextUrl.pathname = `/en${pathname}`;
+    return Response.redirect(req.nextUrl);
+  },
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api)(.*)"],
