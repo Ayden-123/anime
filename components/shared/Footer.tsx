3372b733b1a873228875a5afe28c1b7de51f5fde
qfxla <84011519+qfxla@users.noreply.github.com>
2024-04-20 19:39:20 +0800

    add friendlink

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -23,7 +23,13 @@ const Footer = ({ lang, dict }: { lang: string; dict: any }) => {
               </div>
               <div className="mb-5 mr-8 flex max-w-[200px] grow basis-[100px] flex-col space-y-5 lg:mx-10">
                 <div className="font-inter font-medium">FRIENDS</div>
-                
+                <a
+                    href="https://would-you-rather"
+                    target="_blank"
+                    className="font-inter font-light text-gray-500"
+                  >
+                    Would-You-Rather
+                  </a>
               </div>
               <div className="mb-5 mr-8 flex max-w-[200px] grow basis-[100px] flex-col space-y-5 lg:mx-10">
                 <div className="font-inter font-medium">CREDIT TO</div>
